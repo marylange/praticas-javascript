3400d2a2a6ba8e037224df0fd649092fd6f45068
Marylange <marylangess@gmail.com>
2020-06-03 12:24:24 -0300

    organizando arquivos

--- testeMary8/teste1.js
+++ testeMary8/teste1.js
@@ -1,14 +1,30 @@
-fetch("apiSaga.json")
+const api = 'apiSaga.json';
+fetch(api)
   .then(response => response.json())
   .then(data => {
-    console.log(data);
-    return data;
+    render(data);
   });
-
-const initialArticle = (data) => {
-  const info = data[0].map((elemento) => {
+//renderizando o conteúdo
+const render = (data) => {
+  const mapeamentoDados = initialInformation(data);
+  criarHTML(mapeamentoDados);
+}
+//mapeando infos iniciais do site
+const initialInformation = (data) => {
+  const infosAPI = data.sections.map((elemento) => {
     return {
-      creators: elemento.title,
+      autores: elemento.title,
+      descricaoCriadores: elemento.description,
+      imagem: elemento.image,
     }
-  })
+  });
+  console.log('k',infosAPI);
+}
+//criando html para infos iniciais
+const tagArticle = document.getElementsByClassName('lado-esquerdo');
+console.log('tagArticle', tagArticle);
+const criarHTML = (mapeamentoDados) => {
+  for(let i = 0; i < mapeamentoDados.length; i += 1) {
+    const tagSection = document.createElement('section');
+  }
 }
